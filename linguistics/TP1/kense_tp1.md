294da59a7673531e4f719ac9b7caf4e803d4dacf
Kense <ningkense@alumni.wfu.edu>
2020-02-25 09:51:31 +0100

    METL tp1 ups

---
+++ linguistics/TP1/kense_tp1.md
@@ -0,0 +1,14 @@
+# METL TP 1
+Tientso Ning
+
+## Softmax
+1. Clearly redefine the softmax function:
+softmax(X) = exp(x<sub>i</sub>)/\sum_{k=1}^{n}exp(x<sub>i</sub>)
+where X = {x<sub>1</sub>, x<sub>2</sub>, ... , x<sub>n</sub>}
+2. Show that the softmax is invariant to constant offsets.
+(show that using logs, products because addition, and division becomes subtraction,
+  and that because the sum sums over everything, the constant essentially doesn't alter
+  the results of the softmax function)
+3. Please see code.
+
+## 
--- linguistics/TP1/softmax.py
+++ linguistics/TP1/softmax.py
@@ -17,8 +17,8 @@ def softmax(x):
     it helpful for your later problems.
     """
     # YOUR CODE HERE
-    return
-
+    max_i = np.max(x, axis=-1, keepdims=True)
+    return np.exp(x - max_i)/np.sum(np.exp(x - max_i), axis=-1, keepdims=True)
 
 def test_softmax_basic():
     """
@@ -41,7 +41,7 @@ def test_softmax_basic():
     assert np.amax(np.fabs(test3 - np.array(
         [0.73105858, 0.26894142]))) <= 1e-6
 
-    print('You should verify these results!\n')
+    #print('You should verify these results!\n')
 
 
 def test_softmax():
@@ -49,9 +49,33 @@ def test_softmax():
     Use this space to test your softmax implementation by running:
         python3 softmax.py
     """
-    print('Running your tests...')
+    #print('Running your tests...')
     # YOUR CODE HERE
+    from scipy.special import softmax as ss #disclaimer, only for testing purposes
+    import time
 
+    obtained = softmax(np.array([1]))
+    print(obtained)
+    resy = ss(np.array([1]))
+    assert np.amax(np.fabs(obtained - resy) <= 1e-6)
+
+    obtained = softmax(np.array([1,2,3,4,5]))
+    print(obtained)
+    resy = ss(np.array([1,2,3,4,5]))
+    assert np.amax(np.fabs(obtained - resy) <= 1e-6)
+
+    start = time.clock()
+    calc = softmax(np.array([[1000,2,3],[4,5,6],[7,8,9],[1,2,3]]))
+    end = time.clock()
+    our_time = end-start
+    start = time.clock()
+    calc = ss(np.array([[1000,2,3],[4,5,6],[7,8,9],[1,2,3]]))
+    end = time.clock()
+    their_time = end-start
+    consideration_for_speed = our_time - their_time
+    print("time difference: ", consideration_for_speed)
+
+    print("Tests passed")
 
 if __name__ == '__main__':
     test_softmax_basic()
